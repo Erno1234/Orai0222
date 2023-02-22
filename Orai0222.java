835c30502d194bd9d19c6d74cac12f78fb072f52
Erno1234 <palocierno12>
2023-02-22 15:48:00 +0100

    metódusokra bontás

--- Orai0222.java
+++ Orai0222.java
@@ -6,21 +6,20 @@ public class Orai0222 {
 
 
     public static void main(String[] args) {
+        feladatok();
         int osszeg = elso10SzamOsszege();
         String szoveg = String.format("az elso 10 szam osszege: %d", osszeg );
         kiiras(szoveg+"\n");
         
         int a = 5;
         int b = 22;
-        osszeg = osszead(a, b); 
-        double gyok = Math.sqrt(a+b+a);
+        
         szoveg = String.format("%d + %d + %d + %d = %d\n", a, b,a,b, a + b + a + b);
         kiiras(szoveg);
         szoveg = String.format("%d + %d + %d= %f\n",a,b,a, gyok);
         kiiras(szoveg);
-        
     }
-
+    
     private static int elso10SzamOsszege() {
         int osszeg = 0;
         for (int i = 0; i < 10; i++) {
@@ -35,6 +34,16 @@ public class Orai0222 {
 
     private static void kiiras(String eredmeny) {
         System.out.println(eredmeny);
+    }
+
+    private static void gyok() {
+        int osszeg = elso10SzamOsszege();
+        osszeg = Math.sqrt(osszead(2, osszead(3,4))); 
+        double gyok = Math.sqrt(osszeg);
+    }
+
+    private static void feladatok() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
     
