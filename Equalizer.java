215345ecce14c9b2461e3f27eedda8b49e0a1f1e
Erno1234 <palocierno12>
2023-02-24 12:40:55 +0100

    Boolean érték javítás

--- Equalizer.java
+++ Equalizer.java
@@ -1,39 +1,48 @@
-
 package orai0222;
 
 import java.util.Random;
 
-
 public class Equalizer {
-           private static Random rand = new Random();
+
+    private static Random rand = new Random();
+
     public static void main(String[] args) {
-       otSor();
-       
+        otSor();
+
     }
 
-    private static void eq(int szam){
-        for (int i = 0; i <= szam; i++) {
-            System.out.print("\u001B[45m" + " "); 
+    private static void eq() {
+        eq(false);
+    }
+
+    private static void eq(int db) {
+        eq(db, false);
+        
+    }
+
+    private static void eq(boolean ertek) {
+        int db = rand.nextInt(3, 8);
+        eq(db, ertek);
+    }
+
+    private static void eq(int db, boolean ertek) {
+        for (int i = 0; i < db; i++) {
+            System.out.print("\u001B[45m" + " ");
         }
-        System.out.print(" ");
-    }
-    
-    private static void eq(){
-       int velsz = rand.nextInt(3,8);
-       eq(velsz);
-    }
-    private static void eq(boolean ertek){
-        if (ertek == true){
-            eq();
+        System.out.println(" ");
+
+        if (ertek) {
+            System.out.print(" " + "(" + db + ")");
         }
     }
+
     private static void otSor() {
-       eq(12);
-       eq(8, true);
-       eq();
-       eq(false);
-       eq();
-       
+        eq(12);
+        eq(8, true);
+        eq(true);
+        eq(false);
+        eq();
+
     }
-    
+
 }
