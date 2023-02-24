067e726289637f31c1fb2deb50e37dfbd3f40ae3
Erno1234 <palocierno12>
2023-02-24 13:21:43 +0100

    qrkod

---
+++ Qrkod.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package orai0222;
+
+/**
+ *
+ * @author Pálóci Ernő
+ */
+public class Qrkod {
+    
+}
